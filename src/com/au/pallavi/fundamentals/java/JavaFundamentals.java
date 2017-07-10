e05310aa41aacb3295f7033fcd16e61d487be609
Pallavi Madhwal <pallavi.madhwal@accoliteindia.com>
2017-07-10 18:37:45 +0530

    JavaFundamentals test commit.

---
+++ src/com/au/pallavi/fundamentals/java/JavaFundamentals.java
@@ -0,0 +1,26 @@
+package com.au.pallavi.fundamentals.java;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class JavaFundamentals.
+ */
+public class JavaFundamentals {
+
+	/** The var. */
+	static Integer var = 5;
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test java method.");
+		System.out.println(var);
+
+		for (int i = 0; i < 5; i++) {
+			var = var + 1;
+		}
+
+	}
+}
